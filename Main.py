947e6b749c4db7ef9d967e64f2abe00f4c539bee
KriziaHB <kbuck1@cub.uca.edu>
2017-08-01 14:41:45 -0500

    Initial commit

---
+++ Main.py
@@ -0,0 +1,33 @@
+# Student: Krizia Houston Buck
+# Faculty: Dr. Emre Celebi
+# University of Central Arkansas
+# Summer 2017
+# Graduate Project
+
+# Analyze Results
+
+
+
+
+
+
+
+
+
+# Call Everything from Main #
+def main():
+    print("Krizia Houston Buck - UCA Summer 2017 - Graduate Project - Dr. Emre Celebi")
+
+    # new file called Results.txt for all user input and elapsed times
+    output = open("Final_Results.txt", "w")
+    output.write("Krizia Houston Buck - UCA Summer 2017 - Graduate Project - Dr. Emre Celebi \n\n")
+
+    
+# end of main #
+
+
+
+# Call Main #
+if __name__ == '__main__':
+    main()
+# end of program #
